ee7adba347e047ece08f95344b8cf20c2ffd1759
Sri Neha D <srinehad5@gmail.com>
2026-07-16 04:09:36 +0530

    Add: find-peak-element - submission-0

---
+++ Data Structures & Algorithms/find-peak-element/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        
+        int n = nums.length;
+        if(n == 1)
+            return 0;
+
+        if(nums[0] > nums[1])
+            return 0;
+        if(nums[n-1] > nums[n-2])
+            return n-1;
+
+        int l = 1, h = n-2;
+        while(l <= h){
+
+            int mid = (l+h)/2;
+            if(nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1])
+                return mid;
+
+            if(nums[mid-1] < nums[mid])
+                l=mid+1;
+            else
+                h=mid-1;
+        }
+
+        return 0;
+    }
+}
