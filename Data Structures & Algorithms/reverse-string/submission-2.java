bd758ecb94bdf63b02996dc5b4583cec01cf3cad
Sri Neha D <srinehad5@gmail.com>
2026-07-30 12:43:48 +0530

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        
+        Stack<Character> st = new Stack<>();
+        for(char ch : s)
+            st.push(ch);
+        int idx = 0;
+        while(!st.isEmpty()){
+            s[idx] = st.pop();
+            idx++;
+        }
+    }
+}
